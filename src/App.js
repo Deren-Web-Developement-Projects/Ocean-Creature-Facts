4fdd6474f8bd22f99b06f887d234be45ef959632
Deren B <dbozerprogramming@gmail.com>
2022-07-04 12:01:10 -0400

    Setting up the images

--- src/App.js
+++ src/App.js
@@ -9,18 +9,44 @@ const background = (
   <img 
     className="background"
     alt="ocean"
-    src="./images/ocean.jpeg"
+    src="./images/ocean.jpg"
   />
 );
 
+let images = [];
+for(let animal in animals) {
+  images.push(
+    <img 
+      key={animal} 
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role='button'
+      onClick={displayFact}
+    />
+  );
+};
+
+function displayFact(e) {
+  const selectedAnimal = e.target.alt;
+  const animalInfo = animals[selectedAnimal];
+  const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+  const funFact = animalInfo.facts[optionIndex];
+  document.getElementById('fact').innerHTML = funFact;
+}
+
 function App() {
   return (
-      <div>
-        <h1>{title}</h1>
-        <h2>Hello 2</h2>
-        {background}
+    <div>
+    <h1>{title || 'Click an animal for a fun fact'}</h1>
+      {showBackground && background}
+      <p id='fact'></p>
+      <div className='animals'>
+        {images}
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
